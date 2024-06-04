35e70d60168a7efafed9a30ab1977e08efbe972d
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-04 15:36:33 +0900

    JdbcContext #2

--- guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
+++ guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
@@ -10,16 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.poscodx.guestbook.repository.GuestbookRepositoryWithJdbcContext;
 import com.poscodx.guestbook.repository.GuestbookRepositoryWithRawJdbc;
 import com.poscodx.guestbook.vo.GuestbookVo;
 
 @Controller
 public class GuestbookController {
 	@Autowired
-	private GuestbookRepositoryWithRawJdbc guestbookRepository;
+	private GuestbookRepositoryWithRawJdbc guestbookRepository1;
 	
-	//@Autowired
-	//private GuestbookRepositoryWithJdbcContext guestbookRepository2;
+	@Autowired
+	private GuestbookRepositoryWithJdbcContext guestbookRepository2;
 	
 	//@Autowired
 	//private GuestbookRepositoryWithJdbcTemplate guestbookRepository3;
@@ -27,7 +28,7 @@ public class GuestbookController {
 	
 	@RequestMapping("/")
 	public String index(Model model) {
-		List<GuestbookVo> list = guestbookRepository.findAll();
+		List<GuestbookVo> list = guestbookRepository1.findAll();
 		model.addAttribute("list", list);
 		
 		return "index";
@@ -35,7 +36,7 @@ public class GuestbookController {
 
 	@RequestMapping("/add")
 	public String add(GuestbookVo vo) {
-		guestbookRepository.insert(vo);
+		guestbookRepository2.insert(vo);
 		return "redirect:/";
 	}
 
@@ -47,7 +48,7 @@ public class GuestbookController {
 	
 	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
 	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
-		guestbookRepository.deleteByNoAndPassword(no, password);
+		guestbookRepository2.deleteByNoAndPassword(no, password);
 		return "redirect:/";
 	}
 }
--- guestbook3/src/main/java/com/poscodx/guestbook/repository/template/JdbcContext.java
+++ guestbook3/src/main/java/com/poscodx/guestbook/repository/template/JdbcContext.java
@@ -2,30 +2,39 @@ package com.poscodx.guestbook.repository.template;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class JdbcContext {
-	int executeUpdate(StatementStrategy stmt) {
-		int result = 0;
-		
-		
-		try (
-			Connection conn = dataSource.getConnection();
-			PreparedStatement pstmt1 = conn.prepareStatement("insert into guestbook values(null, ?, ?, ?, now())");
-			PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");
-			) {
-			
-			pstmt1.setString(1, vo.getName());
-			pstmt1.setString(2, vo.getPassword());
-			pstmt1.setString(3, vo.getContents());
-			result = pstmt1.executeUpdate();
+import javax.sql.DataSource;
 
-			ResultSet rs = pstmt2.executeQuery();
-			vo.setNo(rs.next() ? rs.getLong(1) : null);
-			rs.close();
+public class JdbcContext {
+	private DataSource dataSource;
+	
+	public JdbcContext(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	public int executeUpdate(StatementStrategy statementStrategy) {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			pstmt = statementStrategy.makeStatement(conn);
+			result = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println("Error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(SQLException e) {
+				System.out.println("Error:" + e);
+			}
 		}
 		
 		return result;		
