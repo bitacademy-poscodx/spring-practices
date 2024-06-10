fb6858379e17687cb511ab8083d33ff42598778a
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-10 13:49:43 +0900

    aop #finish

--- aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
@@ -1,7 +1,10 @@
 package com.poscodx.aoptest.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -24,5 +27,26 @@ public class MyAspect {
 	public void adviceAfterReturning() {
 		System.out.println("--- AfterReturning Advice ---");
 	}
-	
+
+	@AfterThrowing(value="execution(* *..*.*.*(..))", throwing="ex")
+	public void adviceAfterThrowing(Throwable ex) {
+		System.out.println("--- AfterThrowing Advice " + ex + "---");
+	}
+
+	@Around("execution(* *..*.ProductService.*(..))")
+	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
+		/* Before */
+		System.out.println("--- Around(Before) ---");
+		
+		/* Point Cut Method 실행 */
+		// Object[] params = {"Camera"};
+		// Object result = pjp.proceed(params);
+		
+		Object result = pjp.proceed();
+		
+		/* After */
+		System.out.println("--- Around(After) ---");
+		
+		return result;
+	}
 }
